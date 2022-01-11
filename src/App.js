44a0713e7ea59eb7b0452ae9a5d57272ee996142
zahrajiryaee77@gmail.com <zahrajiryaee77@gmail.com>
2022-01-11 14:27:36 +0330

    create user if they dont alredy exist on db & setState

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.comonent";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import "./App.css";
 
@@ -25,10 +25,29 @@ class App extends React.Component {
     // know if the user has signin or out without manually fetch
     // oAuth allows users to signin with any other 3rd party service that they might have (google, facebook, github)
     // this is open subscribtion (open messaging system between our app and firebase)
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        // to check if database has updated at that reference with any new data
+        const userRef = await createUserProfileDocument(userAuth);
+        // onSnapshot represents the data stored on database
+        userRef.onSnapshot((snapshot) => {
+          this.setState(
+            {
+              currentUser: {
+                id: snapshot.id,
+                ...snapshot.data(),
+              },
+            },
+            () => {
+              console.log("state:", this.state);
+            }
+          );
+        });
+      } else {
+        this.setState({ currentUser: userAuth });
+      }
 
-      console.log("user::", user);
+      console.log("currentUser::", userAuth);
     });
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,8 +1,6 @@
 import firebase from "firebase/compat/app";
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
-import { getAnalytics } from "firebase/analytics";
-import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const config = {
@@ -15,20 +13,46 @@ const config = {
   measurementId: "G-G9Z8T4J3B6",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  // query inside the fireStore to see if it already exist
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShop = await userRef.get();
+
+  if (!snapShop.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.error("error creating user::", error.message);
+    }
+  }
+
+  return userRef;
+};
+
 firebase.initializeApp(config);
+
+// export const auth = firebase.auth();
+export const auth = getAuth();
+export const firestore = firebase.firestore();
 
 // const provider = new firebase.auth.GoogleAuthProvider();
 const provider = new GoogleAuthProvider();
-// provider.setCustomParameters({ prompt: "select_account" });
 provider.setCustomParameters({
   prompt: "select_account",
-});
+}); /* this triggers the google pop up */
 
-//
-export const firestore = firebase.firestore();
-// export const auth = firebase.auth();
-export const auth = getAuth();
-// export const signInWithGoogle = auth.signInWithPopup(provider);
+// export const signInWithGoogle = () => auth.signInWithPopup(provider);
 export const signInWithGoogle = async () => {
   return await signInWithPopup(auth, provider);
 };
