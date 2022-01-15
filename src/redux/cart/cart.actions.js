3c8fd6362aeddd509500d00563aa8a8201597ccc
z.jiryaee <zahrajiryaee@gmail.com>
2022-01-15 17:19:11 +0330

    implement addItem reducer -- without grouping items

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -2,6 +2,9 @@ import CartActionTypes from "./cart.types";
 
 export const toggleCartHidden = () => ({
   type: CartActionTypes.TOGGLE_CART_HIDDEN,
-});
+}); // we don't need payload here
 
-// we don't need payload here
+export const addItem = (item) => ({
+  type: CartActionTypes.ADD_ITEM,
+  payload: item,
+});
