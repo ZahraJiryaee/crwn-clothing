a3b9ffbb728fe8f3e7ac75b12e361fcb4e7f38dd
z.jiryaee <zahrajiryaee@gmail.com>
2022-01-15 14:44:28 +0330

    add user action types to avoid accidental typos

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,7 +1,9 @@
 // functions that return object
 // each object is in the correct format that the action is expected to be
 
+import { UserActionTypes } from "./user.types";
+
 export const setCurrentUser = (user) => ({
-  type: "SET_CURRENT_USER",
+  type: UserActionTypes.SET_CURRENT_USER,
   payload: user,
 });
