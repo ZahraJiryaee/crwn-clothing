9f82ed06e59b3061a615f17a6b784cf7139000c3
z.jiryaee <zahrajiryaee@gmail.com>
2022-01-15 19:12:04 +0330

    updating the app with reselect, replaced cart selectors with memoized selectors

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,20 +2,45 @@ import React from "react";
 import { connect } from "react-redux";
 
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selector";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className="cart-icon" onClick={toggleCartHidden}>
     <ShoppingIcon className="shopping-icon" />
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
+
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
 
 const mapDispatchToProps = (dispatch) => ({
   toggleCartHidden: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
+
+/****************** why we need reselect ******************
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+    0
+  ),
+});
+
+selector: gets the whole state & pulls of a small portion of it
+
+whenever we return a new obj from a redux & redux recompiles & rebuils the entire state obj, 
+mapStateToProps is getting called every single time which is always passing new props to the component
+this is always rerendering our component.
+reduce alwats return a new value
+
+we want to store or cache the value.
+so if the cartItem values don't actually change & the output of the selector doesn't change, 
+we don't wanna rerender the component => memoization: caching the selector value
+*/
---
+++ src/redux/cart/cart.selector.js
@@ -0,0 +1,35 @@
+import { createSelector } from "reselect";
+
+const selectCart = (state) => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  (cart) => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  (cartItems) =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
+
+/*
+1.input selector:
+doesn't use createSelector
+nameing convension: "selectName"
+its a func that gets the whole state & just returns a slice of it
+
+2.output selector:
+does use input selectors & createSelector to build them selves
+nameing convension: "selectCartItems" => cartItems is property in cart
+createSelector(
+    collection of input selecters, 
+    a func that return the value we want out of the selector--
+        parameters: each output of the input selectors array in the order that the selectors were written
+)
+bc of the createSelector, selectCartItems is now a memoized selector
+*/
