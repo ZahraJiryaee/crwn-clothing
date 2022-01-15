03607b545776a64992ec85df988676c4b722dafe
z.jiryaee <zahrajiryaee@gmail.com>
2022-01-15 17:53:33 +0330

    updated cart utils to group items in cart reducer

---
+++ src/redux/cart/cart.utils.js
@@ -0,0 +1,21 @@
+export const addItemToCart = (cartItems, cartItemToAdd) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.id === cartItemToAdd.id
+  );
+  /*
+   find  will return the first item found in the array based of the condition that we pass.
+   if it matches, it will sent that cartItem to the constant.
+   if it doesn't find anything, it returns undefined.
+  */
+
+  if (existingCartItem) {
+    // map returns a new array -- we should always return a new version of the state
+    return cartItems.map((cartItem) =>
+      cartItem.id === cartItemToAdd.id
+        ? { ...cartItem, quantity: cartItem.quantity + 1 }
+        : cartItem
+    );
+  }
+  return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
+  // quantity property gets attached the first time around since the if block won't run when its a new item
+};
