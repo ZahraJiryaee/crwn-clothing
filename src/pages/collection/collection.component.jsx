a66a24ab4ba0369ec84ce33a7e5908575f3ea303
zahrajiryaee77@gmail.com <zahrajiryaee77@gmail.com>
2022-01-18 10:33:42 +0330

    added collection-page & nested routing in shops component

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import { selectCollection } from "../../redux/shop/shop.selector";
+
+import "./collection.styles.scss";
+
+const CollectionPage = ({ collection }) => {
+  console.log("collection::", collection);
+  return (
+    <div className="collection-page">
+      <h2>Collection PAGE</h2>
+    </div>
+  );
+};
+
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
+});
+
+export default connect(mapStateToProps)(CollectionPage);
+
+/*
+mapStateToProps(
+  state: overall reducer state from the top,
+  ownProps: the props of the component that we're wrapping in connect
+)
+*/
+
+/*
+# we shoul pass state to the selector manually when we dont use createStructuredSelector
+this is necessary bc unlike other selectors, this selector needs a partt of the state,
+depending on the url parameter
+so we pass state as the second parameter
+selectCollection 
+*/
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { Route } from "react-router-dom";
 
 import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = () => (
+const ShopPage = ({ match }) => (
   <div className="shop-page">
-    <CollectionsOverview />
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
   </div>
 );
 
 export default ShopPage;
+
+/*
+bc in App.js Shop in nested in a Route, 
+and Route automatically passes match, history, location
+
+for thae same reason CollectionPage has access to match
+*/
