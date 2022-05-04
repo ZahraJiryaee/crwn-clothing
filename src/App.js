ab079a909aafb53ee0ee0b22b58a4fe7f2850b27
zahrajiryaee77@gmail.com <zahrajiryaee77@gmail.com>
2022-05-04 19:07:35 +0430

    refactor user saga

--- src/App.js
+++ src/App.js
@@ -10,9 +10,6 @@ import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
 
-import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
-
-import { setCurrentUser } from "./redux/user/user.actions";
 import { selectCurrentUser } from "./redux/user/user.selector";
 
 import "./App.css";
@@ -21,8 +18,6 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser } = this.props;
-
     // this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
     //   if (userAuth) {
     //     const userRef = await createUserProfileDocument(userAuth);
@@ -35,7 +30,6 @@ class App extends React.Component {
     //   } else {
     //     setCurrentUser(userAuth);
     //   }
-
     //   console.log("currentUser::", userAuth);
     // });
   }
@@ -70,8 +64,4 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
 });
 
-const mapDispatchToProps = (dispatch) => ({
-  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
