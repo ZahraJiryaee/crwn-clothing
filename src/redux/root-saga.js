8e51a0a894f95ebe17333d92c4a80d2e563ad797
zahrajiryaee77@gmail.com <zahrajiryaee77@gmail.com>
2022-05-05 18:08:51 +0430

    clear cart saga on user sign out

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -2,9 +2,10 @@ import { all, call } from "redux-saga/effects";
 
 import { fetchCollectionsStart } from "./shop/shop.sagas";
 import { userSaga } from "./user/user.sagas";
+import { cartSagas } from "./cart/cart.sagas";
 
 export default function* rootSaga() {
-  yield all([call(fetchCollectionsStart), call(userSaga)]);
+  yield all([call(fetchCollectionsStart), call(userSaga), call(cartSagas)]);
 }
 
 /*
